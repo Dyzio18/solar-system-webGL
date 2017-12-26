f19036b37c8eef0ad5af5b9ba3e0703e8f62a730
dyzio18 <dyzio18@gmail.com>
2017-12-27 00:06:19 +0100

    add texture

--- src/SolarSystem.js
+++ src/SolarSystem.js
@@ -1,33 +1,92 @@
 import * as THREE from 'three';
 
-const AU = 1;
+const AU = 2;
+// To TEST
+const API = {
+    offsetX: 0,
+    offsetY: 0,
+    repeatX: 0.25,
+    repeatY: 0.25,
+    rotation: Math.PI / 4, // positive is counter-clockwise
+    centerX: 0.5,
+    centerY: 0.5
+};
 
-const solarSystemCreate = (scene, planets) => {
-    let geometry = new THREE.SphereBufferGeometry(earth.radius, 32, 32);
-    let material = new THREE.MeshNormalMaterial();
-    let earthPlanet = new THREE.Mesh( geometry, material );
+const solarSystemCreate = (scene, planets, render) => {
+
+    let loader = new THREE.TextureLoader();
+    let texture;
+
+    // [3] Earth
+    texture = loader.load('https://raw.githubusercontent.com/BrockBeldham/threejs-solar-system-experiment/master/static/img/earth.jpg', render);
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.matrixAutoUpdate = false;
+    let earthPlanet = new THREE.Mesh(new THREE.SphereBufferGeometry(earth.radius, 32, 32), new THREE.MeshBasicMaterial({map: texture}));
+    //earthPlanet.overdraw = true;
+    //earthPlanet.castShadow = true;
     scene.add(earthPlanet);
     planets.earth = earthPlanet;
+
+    // [4] Mars
+    texture = loader.load('https://raw.githubusercontent.com/BrockBeldham/threejs-solar-system-experiment/master/static/img/mars.jpg', render);
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.matrixAutoUpdate = false;
+    let marsPlanet = new THREE.Mesh(new THREE.SphereBufferGeometry(mars.radius, 32, 32), new THREE.MeshBasicMaterial({map: texture}));
+    scene.add(marsPlanet);
+    planets.mars = marsPlanet;
+
+    //  [5] Jupiter
+    texture = loader.load('https://raw.githubusercontent.com/BrockBeldham/threejs-solar-system-experiment/master/static/img/jupiter.jpg', render);
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.matrixAutoUpdate = false;
+    let jupiterPlanet = new THREE.Mesh(new THREE.SphereBufferGeometry(jupiter.radius, 32, 32), new THREE.MeshBasicMaterial({map: texture}));
+    scene.add(jupiterPlanet);
+    planets.jupiter = jupiterPlanet;
+
 };
 
 const solarSystemMove = (planets) => {
 
-    /*venus.position.x = Math.cos(radians) * venusOrbitRadius;
-    venus.position.z = Math.sin(radians) * venusOrbitRadius;*/
-    earth.orbit -= Math.PI/180;
-    planets.earth.position.x +=  Math.cos(earth.orbit) * earth.distance;
-    planets.earth.position.z +=  Math.sin(earth.orbit) * earth.distance;
+    // [3] Earth
+    earth.orbit += earth.lineSpeed;
+    planets.earth.position.x = Math.cos(earth.orbit) * earth.distance;
+    planets.earth.position.z = Math.sin(earth.orbit) * earth.distance;
 
-    // planets.earthPlanet.position.x += 0.01;
+    // [4] Mars
+    mars.orbit += mars.lineSpeed;
+    planets.mars.position.x = Math.cos(mars.orbit) * mars.distance;
+    planets.mars.position.z = Math.sin(mars.orbit) * mars.distance;
+
+    //  [5] Jupiter
+    jupiter.orbit += jupiter.lineSpeed;
+    planets.jupiter.position.x = Math.cos(jupiter.orbit) * jupiter.distance;
+    planets.jupiter.position.z = Math.sin(jupiter.orbit) * jupiter.distance;
+
 };
 
-
+const mars = {
+    radius: 1,
+    distance: AU + 6,
+    circulationTime: 365,
+    lineSpeed: (2 * Math.PI / 2000) * AU,
+    orbit: 2 * Math.PI * AU * AU
+};
 
 const earth = {
     radius: 1,
-    distance: AU,
+    distance: AU + 2,
     circulationTime: 365,
-    orbit: 365 *  Math.PI / 180
+    lineSpeed: (2 * Math.PI / 1000) * AU,
+    orbit: 2 * Math.PI * AU * AU
 };
+
+const jupiter = {
+    radius: 3,
+    distance: 15.6 * AU,
+    circulationTime: 365,
+    lineSpeed: (2 * Math.PI / 11000) * AU,
+    orbit: 2 * Math.PI * AU * AU
+};
+
 
 export {solarSystemCreate, solarSystemMove}
--- src/main.js
+++ src/main.js
@@ -3,42 +3,48 @@ import OrbitControls from 'orbit-controls-es6';
 import {solarSystemCreate, solarSystemMove} from './SolarSystem';
 import {helpers} from './Helpers';
 
-const HELLO = "hello";
-console.log(`${HELLO} world`);
-
-let camera, scene, renderer, controls;
-let planets = {mercury: {}, venus:{}, earth: {}};
+let camera, scene, renderer, controls, light;
+let planets = {mercury: {}, venus: {}, earth: {}, mars: {}, jupiter: {}};
 
 init();
 animate();
 window.addEventListener("resize", resize);
 
-
-
-function init(){
+function init() {
     // Create scene
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
     // Camera initialization
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.04, 1000);
     camera.position.z = 1;
 
     // Render container
-    const container = document.getElementById( 'canvas' );
-    document.body.appendChild( container );
-    renderer =  new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize( window.innerWidth*0.9, window.innerHeight*0.9);
-    container.appendChild( renderer.domElement );
+    const container = document.getElementById('canvas');
+    document.body.appendChild(container);
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9);
+    container.appendChild(renderer.domElement);
 
     // Camera control
-    controls = new OrbitControls( camera, container );
+    controls = new OrbitControls(camera, container);
+
+
+    // Light - Sun
+    light = new THREE.SpotLight();
+    light.castShadow = true;
+    light.position.set(0, 0, 0);
+    light.target = scene;
+    scene.add(light);
+
 
     // Some helpers (axis, grid)
     helpers(scene);
     // Create Solar System
-    solarSystemCreate(scene, planets);
+    solarSystemCreate(scene, planets, render());
+
 
     // Camera
-    camera.position.set( 0, 5, 5 );
+    camera.position.set(0, 5, 5);
     controls.update();
 
 
@@ -58,8 +64,16 @@ function animate() {
     controls.update();
     solarSystemMove(planets);
 
+    render();
+}
+
+function render() {
     renderer.render(scene, camera);
 }
+
+
+
+
 
 
 
